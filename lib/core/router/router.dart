aa598f9de79fdfa8128aeb63d2e0b7b99b2e00d5
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-08-17 21:36:47 +0900

    ADD : Android v1 embedding 오류 해결 전을 위한 커밋

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,6 +1,13 @@
 import 'package:dog_ibw_calulator/presentation/main/loading_screen.dart';
 import 'package:dog_ibw_calulator/presentation/main/main_screen.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:flutter/material.dart';
+
+Future<InitializationStatus> _initGoogleMobileAds() {
+  // TODO: Initialize Google Mobile Ads SDK
+  return MobileAds.instance.initialize();
+}
 
 final router = GoRouter(
   routes: [
--- lib/main.dart
+++ lib/main.dart
@@ -2,55 +2,123 @@ import 'package:dog_ibw_calulator/presentation/main/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+const Map<String, String> UNIT_ID = kReleaseMode
+    ? {
+        'ios': '[YOUR iOS AD UNIT ID]',
+        'android': '[YOUR ANDROID AD UNIT ID]',
+      }
+    : {
+        'ios': 'ca-app-pub-3940256099942544/2934735716',
+        'android': 'ca-app-pub-3940256099942544/6300978111',
+      };
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadData(); //
-  }
-
-  void _loadData() async {
-    await Future.delayed(const Duration(seconds: 5)); //
-    setState(() {
-      _isLoading = false;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: _isLoading
-          ? Scaffold(
-              body: Center(
-                child: Lottie.asset(
-                  'assets/lottie/animation_lkuuqz8b.json',
-                  width: 200,
-                  height: 200,
-                ),
-              ),
-            )
-          : const MainScreen(),
+      debugShowCheckedModeBanner: false,
+      home: const _MyAppState(),
     );
+  }
+}
+
+class _MyAppState extends StatefulWidget {
+  const _MyAppState({Key? key}) : super(key: key);
+
+  @override
+  State<_MyAppState> createState() => _MyAppStateState();
+}
+
+class _MyAppStateState extends State<_MyAppState> {
+  bool _isLoading = true;
+  late BannerAd _bannerAd;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAd();
+    _loadData();
+  }
+
+  void _loadAd() {
+    TargetPlatform os = Theme.of(context).platform;
+
+    _bannerAd = BannerAd(
+      listener: BannerAdListener(
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          print('Ad failed to load: $error');
+        },
+        onAdLoaded: (Ad ad) {
+          print('Ad loaded: $ad');
+        },
+      ),
+      size: AdSize.banner,
+      adUnitId: UNIT_ID[os == TargetPlatform.iOS ? 'ios' : 'android']!,
+      request: const AdRequest(),
+    );
+
+    _bannerAd.load();
+  }
+
+  void _loadData() async {
+    await Future.delayed(const Duration(seconds: 5));
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    _bannerAd.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    return _isLoading
+        ? Scaffold(
+            body: Center(
+              child: Lottie.asset(
+                'assets/lottie/animation_lkuuqz8b.json',
+                width: 200,
+                height: 200,
+              ),
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text('Admob'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: const MainScreen(),
+                  ),
+                  Container(
+                    height: 50,
+                    child: AdWidget(ad: _bannerAd),
+                  ),
+                ],
+              ),
+            ),
+          );
   }
 }
