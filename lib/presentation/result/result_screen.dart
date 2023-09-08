3368a096dcb8afd5304412d970d142e7c3f0c9ef
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-09-08 17:13:04 +0900

    ADD : 하단에 광고 나오게 수정

--- lib/presentation/result/result_screen.dart
+++ lib/presentation/result/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class ResultScreen extends StatelessWidget {
   final double weight;
@@ -53,6 +54,17 @@ class ResultScreen extends StatelessWidget {
     final imageWidget = _buildImage(ibw);
     final weightStatus = _getWeightStatus(ibw);
 
+    // 광고 로딩 및 배너 광고 객체 생성
+    BannerAd _bannerAd = BannerAd(
+      adUnitId: 'ca-app-pub-1136520834945848~2460543854', // 광고 단위 아이디 설정
+      size: AdSize.banner,
+      request: const AdRequest(), // 광고 요청 설정
+      listener: const BannerAdListener(), // 광고 리스너 설정
+    );
+
+    // 광고 로드
+    _bannerAd.load();
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
@@ -71,7 +83,7 @@ class ResultScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 200), // 추가된 부분, AppBar 높이만큼 여백 추가
+                const SizedBox(height: 200),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -84,36 +96,32 @@ class ResultScreen extends StatelessWidget {
                       '현재 체중 : $weight kg',
                       style: const TextStyle(fontSize: 25),
                     ),
-                    // 추가된 부분, 체중 상태 표시
-                    const SizedBox(width: 16), // 현재 체중과 표준 체중 사이 간격
+                    const SizedBox(width: 16),
                     Text(
                       '표준 체중 : ${ibw.toStringAsFixed(1)} kg',
                       style: const TextStyle(fontSize: 25),
                     ),
                   ],
                 ),
-
                 imageWidget,
-
                 Container(
-                  width: 100, //
+                  width: 100,
                   child: Column(
                     children: [
-                      //
                       Container(
                         decoration: const BoxDecoration(
-                          color: Colors.orangeAccent, // 배경색상 설정
-                          shape: BoxShape.circle, //
+                          color: Colors.orangeAccent,
+                          shape: BoxShape.circle,
                         ),
                         child: IconButton(
                           icon: const Icon(Icons.home),
-                          color: Colors.white, // 아이콘 색상 설정
+                          color: Colors.white,
                           onPressed: () {
                             Navigator.pop(context);
                           },
                         ),
                       ),
-                      const SizedBox(height: 8), // 아이콘과 이미지 사이 간격
+                      const SizedBox(height: 8),
                     ],
                   ),
                 ),
@@ -121,6 +129,10 @@ class ResultScreen extends StatelessWidget {
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: Container(
+        height: 50,
+        child: AdWidget(ad: _bannerAd),
       ),
     );
   }
